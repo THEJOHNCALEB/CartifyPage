98f753786ed2ba70624c10cfc29b85656fa412c4
John Caleb <Calebjohn3112@gmail.com>
2024-06-04 15:58:46 +0100

    Fixed "Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`.  react/no-unescaped-entities"

--- components/home/demo-modal.tsx
+++ components/home/demo-modal.tsx
@@ -30,7 +30,7 @@ const DemoModal = ({
   <div className="flex items-center border-b border-teal-500 py-2">
     <input className="appearance-none bg-transparent border-transparent w-full h-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-gray-100 focus:border-slate-50" type="email" placeholder="John@gmail.com" aria-label="Email"/>
     <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded focus:border-none" type="button">
-      Let's Go
+      Join
     </button>
   </div>
 </form>
